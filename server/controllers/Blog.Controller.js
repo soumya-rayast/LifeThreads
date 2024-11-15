443cde0a69a83aa147d8e594d07550936adcbef2
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-11-15 10:33:58 +0530

    Completed Blog Controller

--- server/controllers/Blog.Controller.js
+++ server/controllers/Blog.Controller.js
@@ -1,7 +1,7 @@
 const Blog = require('../model/Blog.Model')
+const User = require("../model/User.model.js")
 
-
-// create blog
+//function for create blog
 const createBlog = async (req, res) => {
     try {
         // Extracting authorId,from req.id and blog details from req.body
@@ -15,7 +15,7 @@ const createBlog = async (req, res) => {
             category,
             author: authorId,
             createdAt: new Date()
-        }),
+        })
         // saved the blog to the database
         const savedBlog = await newBlog.save();
         res.status(201).json({ message: "Blog Created Successfully", blog: savedBlog });
@@ -24,13 +24,12 @@ const createBlog = async (req, res) => {
     }
 
 }
-
-// get blogs
+// function for get blogs
 const getBlogs = async (req, res) => {
     try {
         const authorId = req.id;
         const blog = await Blog.find({ authorId });
-        if (!blog || getBlogs.length === 0) {
+        if (!blog || blog.length === 0) {
             return res.status(404).json({ message: "No blogs found", success: false })
         }
 
@@ -40,8 +39,7 @@ const getBlogs = async (req, res) => {
         res.status(500).jsn({ message: "Error fetching blogs" })
     }
 }
-
-// get blog by id
+//function  get blog by id
 const getBlogById = async (req, res) => {
     try {
         const blogId = req.params.id;
@@ -60,6 +58,7 @@ const getBlogById = async (req, res) => {
             res.status(500).json({ message: 'Error fetching blog', error: error.message });
     }
 }
+// function for update blog by author
 const updateBlog = async (req, res) => {
     try {
         const { id } = req.params;
@@ -89,5 +88,110 @@ const updateBlog = async (req, res) => {
         });
     }
 }
+//function for delete blog
+const deleteBlog = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const blog = await Blog.findById(id);
+        if (!blog) {
+            return res.status(404).json({ message: "Blo not found", success: false })
+        }
+        const { authorId } = blog;
+        await User.findByIdAndUpdate(authorId, {
+            $pull: { uploads: id }
+        });
+        // delete from the database
+        await Blog.findByIdAndDelete(id);
+        return res.status(200).json({ success: false, message: error.message })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: error.message })
+    }
+}
+//function for search blog
+const searchBlog = async (req, res) => {
+    const { search } = req.query;
+    try {
+        const blogs = await Blog.find({
+            $or: [
+                { title: { $regex: search, $options: "i" } },
+                { content: { $regex: search, $options: "i" } },
+                { tags: { $regex: search, $options: "i" } },
+                { category: { $regex: search, $options: "i" } }
+            ]
+        });
+        if (blogs.length === 0) {
+            return res.status(404).json({ success: false, message: "No Blogs Found" })
+        }
+        return res.status(200).json({ success: true, data: blogs })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ success: false, message: error.message })
+    }
+}
+// function for add to favourites   
+const addToFavourites = async (req, res) => {
+    const { blogId } = req.params;
+    const userId = req.id;
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+        if (user.favourites.includes(blogId)) {
+            return res.status(400).json({ success: false, message: "Blog already in favourites" })
+        }
+        user.favourites.push(blogId);
+        await user.save();
+        return res.status(200).json({ success: true, message: "Blog added to favourites" });
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ success: false, message: error.message })
+    }
+}
+// function for remove Blog from favourites  
+const removefromfavourites = async (req, res) => {
+    const userId = req.id;
+    const { blogId } = req.params;
+    try {
+        const user = await User.findByIdAndUpdate(
+            userId,
+            { new: true }
+        )
+        if (!user) {
+            return res.status(404).json({ success: false, message: "user not found" })
+        }
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ success: false, message: error.message })
+    }
+}
+// function for get favourites 
+const getFavourites = async (req, res) => {
+    const userId = req.id;
+    try {
+        const user = await User.findById(userId).populate('favorites');
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" })
+        }
+        const { favourites } = user;
+        if (!favourites || favourites.length === 0) {
+            return res.status({ success: false, message: "No favourites added" });
+        }
+        return res.status(200).json({ success: true, data: favourites })
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message })
+    }
+}
 
-// for delete blog
+module.exports = {
+    createBlog,
+    getBlogs,
+    getBlogById,
+    updateBlog,
+    deleteBlog,
+    searchBlog,
+    addToFavourites,
+    removefromfavourites,
+    getFavourites
+}
